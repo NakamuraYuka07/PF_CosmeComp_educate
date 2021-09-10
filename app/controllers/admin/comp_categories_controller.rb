c6a99daa6bc5118d7c6c24641d46d58c43a88e2a
NakamuraYuka07 <yukaron.8107@gmail.com>
2021-09-10 08:24:40 +0000

    成分一覧、コントローラ作成

--- app/controllers/admin/comp_categories_controller.rb
+++ app/controllers/admin/comp_categories_controller.rb
@@ -1,2 +1,37 @@
-class Admin::CompCategoryController < ApplicationController
+class Admin::CompCategoriesController < ApplicationController
+    before_action :authenticate_admin!
+
+  def index
+    @comp_category = CompCategory.new
+    @comp_categories = CompCategory.all
+  end
+
+  def create
+    @comp_category = CompCategory.new(comp_category_params)
+    if @comp_category.save
+      redirect_back(fallback_location: root_path)
+    else
+      @comp_categories = CompCategory.all
+      render :index
+    end
+  end
+
+  def edit
+    @comp_category = CompCategory.find(params[:id])
+  end
+
+  def update
+    @comp_category = CompCategory.find(params[:id])
+    if @comp_category.update(comp_category_params)
+      redirect_to admin_comp_categories_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def comp_category_params
+    params.require(:comp_category).permit(:name)
+  end
 end
