7f9d41fdb78617234e81cbc8e40d2ac827be3272
NakamuraYuka07 <yukaron.8107@gmail.com>
2021-09-12 17:02:56 +0000

    利用者作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,28 @@
 class Admin::UsersController < ApplicationController
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def index
+    @users = User.all
+    #@users = User.all.order(:id)
+    @userr = User.page(params[:page]).per(10)
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end 
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_user_path(@user)
+  end
+  
+  protected
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :cust_postal_code, :cust_address, :phone_number, :email, :is_deleted)
+  end
 end
