ba19e1dffed603614ef7ed80d9213676ffe5cd8d
NakamuraYuka07 <yukaron.8107@gmail.com>
2021-09-17 18:03:12 +0000

    商品新規登録修正中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -15,9 +15,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    item.save
-    redirect_to admin_items_path
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   def edit
