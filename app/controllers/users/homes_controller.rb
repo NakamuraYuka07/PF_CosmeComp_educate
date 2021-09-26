448a68c3d2f064d96ef8fb6577e9898a8bd89386
NakamuraYuka07 <yukaron.8107@gmail.com>
2021-09-26 13:47:31 +0000

    検索機能作成

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -4,45 +4,49 @@ class Users::HomesController < ApplicationController
   end
 
   def search
+    keywaords_str = "#{params[:keyword]} #{params[:comp_keyword]}"
     # キーワード分割
-    keywords = params[:keyword].split(/[[:blank:]]+/).select(&:present?)
+    if keywaords_str.present?
+      keywords = keywaords_str.split(/[[:blank:]]+/).select(&:present?)
 
-    # 普通のキーワードとマイナスのキーワードを分ける
-    negative_keywords, positive_keywords =
-      keywords.partition { |keyword| keyword.start_with?("-") }
+      # 普通のキーワードとマイナスのキーワードを分ける
+      negative_keywords, positive_keywords =
+        keywords.partition { |keyword| keyword.start_with?("-") }
+      
+      @items = Item.all
 
-    # Itemモデルオブジェクト作成
-    @items = Item
+      # 商品検索ワードの数だけand検索を行う
+      positive_keywords.each do |keyword|
+        @items = @items.where("name LIKE ?", "%#{keyword}%")
+      end
+  
+      # マイナスキーワードの先頭から-を取り除く
+      negative_keywords.each { |word| word.slice!(/^-/) }
+  
+      # マイナスキーワードでnot検索
+      negative_keywords.each do |keyword|
+        next if keyword.blank?
+        @items = @items.where.not("component LIKE ?", "%#{keyword}%")
+      end
+      
+      @components = Component
 
-    # 検索ワードの数だけand検索を行う
-    positive_keywords.each do |keyword|
-      @items = @items.where("name LIKE ?", "%#{keyword}%")
+      # 検索ワードの数だけand検索を行う
+      positive_keywords.each do |keyword|
+        @components = @components.where("name LIKE ?", "%#{keyword}%")
+      end
+
+      # マイナスキーワードの先頭から-を取り除く
+      negative_keywords.each { |word| word.slice!(/^-/) }
+    
+      # マイナスキーワードでnot検索
+      negative_keywords.each do |keyword|
+        next if keyword.blank?
+        @components = @components.where.not("name LIKE ?", "%#{keyword}%")
+      end
+    else
+      @items = Item.all
     end
 
-    # マイナスキーワードの先頭から-を取り除く
-    negative_keywords.each { |word| word.slice!(/^-/) }
-
-    # マイナスキーワードでnot検索
-    negative_keywords.each do |keyword|
-      next if keyword.blank?
-    @items = @items.where.not("name LIKE ?", "%#{keyword}%")
-    end
-
-   @components = Component
-
-    # 検索ワードの数だけand検索を行う
-    positive_keywords.each do |keyword|
-    @components = @components.where("name LIKE ?", "%#{keyword}%")
-    end
-
-   # マイナスキーワードの先頭から-を取り除く
-   negative_keywords.each { |word| word.slice!(/^-/) }
-
-    # マイナスキーワードでnot検索
-    negative_keywords.each do |keyword|
-     next if keyword.blank?
-     @components = @components.where.not("name LIKE ?", "%#{keyword}%")
-    end
   end
-
 end
