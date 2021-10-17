ca30f49e763ccdab31dea263e0623bd9b0729aec
NakamuraYuka07 <yukaron.8107@gmail.com>
2021-10-17 14:23:42 +0000

    コメント機能修正

--- app/controllers/users/items_controller.rb
+++ app/controllers/users/items_controller.rb
@@ -2,8 +2,6 @@ class Users::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).per(10)
     @rank_items = Item.includes(:favorited_users).limit(5).sort { |a,b| b.favorited_users.size <=> a.favorited_users.size }
-    # @rank_items = Item.find(Favorite.group(:item_id).order('count(item_id) desc').limit(5).pluck(:item_id))
-    @categories = ItemCategory.all
   end
 
   def show
